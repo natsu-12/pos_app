85be742047a2df86aba900b002eb293f86abbbc0
kazuya <kazunatsu1218@gmail.com>
2025-12-28 11:24:45 +0900

    update

--- src/data/products.js
+++ src/data/products.js
@@ -1,11 +1,10 @@
 export const products = [
-    {id : 1, name : "ニドサンド", unit_price : 300},
-    {id : 2, name : "レモンケーキ", unit_price : 300},
-    {id : 3, name : "ブラウニー", unit_price : 300},
-    {id : 4, name : "ベリーミール", unit_price : 300},
-    {id : 5, name : "マドレーヌ", unit_price : 300},
-    {id : 6, name : "ディアマン", unit_price : 300},
-    {id : 7, name : "パヴェフレーズ", unit_price : 300},
-    {id : 8, name : "ノワゼット", unit_price : 300}
+  { id: 1, name: "ディアマンショコラ", price: 200 },
+  { id: 2, name: "ベリーミール", price: 200 },
+  { id: 3, name: "レモンケーキ", price: 300 },
+  { id: 4, name: "ニドサンド", price: 300 },
+  { id: 5, name: "マドレーヌ", price: 300 },
+  { id: 6, name: "ブラウニー", price: 300 },
+  { id: 7, name: "クッキー缶（＋ショコラ）", price: 2500 },
+  { id: 8, name: "クッキー缶（＋カネル）", price: 2500 }
 ];
-
