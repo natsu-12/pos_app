864f1eca366ac53c1b730f84dc9e2e987c21a83f
kazuya <kazunatsu1218@gmail.com>
2026-02-21 14:29:33 +0900

    addProducts

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,5 @@
 export const products = [
-  { id: 1, name: "ディアマンショコラ", price: 200 },
-  { id: 2, name: "ベリーミール", price: 200 },
-  { id: 3, name: "レモンケーキ", price: 300 },
-  { id: 4, name: "ニドサンド", price: 300 },
-  { id: 5, name: "マドレーヌ", price: 300 },
-  { id: 6, name: "ブラウニー", price: 300 },
-  { id: 7, name: "クッキー缶（＋ショコラ）", price: 2500 },
-  { id: 8, name: "クッキー缶（＋カネル）", price: 2500 }
+  { id: 1, name: "レモンケーキ", price: 300 },
+  { id: 2, name: "ニドサンド", price: 300 },
+  { id: 3, name: "おつまみナッツ", price: 2500 }
 ];
