edde373ad2e57c8974bdf3e4fb13ca31d7dc8a5b
kazuya <kazunatsu1218@gmail.com>
2026-05-15 08:17:13 +0900

    update data6

--- src/data/products.js
+++ src/data/products.js
@@ -2,6 +2,6 @@ export const products = [
   { id: 1, name: "レモンケーキ", price: 300 },
   { id: 2, name: "ニドサンド", price: 300 },
   { id: 3, name: "おつまみナッツ", price: 300 },
-  { id: 4, name: "スティックフロマージュ", price: 300 }
+  { id: 4, name: "スティックフロマージュ", price: 500 }
 ];
 
